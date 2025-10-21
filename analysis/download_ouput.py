3e453da1bbf2577f144d28db9bd49a5f6651e441
kamp <fbn.kamp@gmail.com>
2025-10-21 15:35:38 +0200

    update data download

--- analysis/download_ouput.py
+++ analysis/download_ouput.py
@@ -11,32 +11,35 @@ password= os.getenv("EXP_PASSWORD")
 root_url = os.getenv("EXP_URL")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--token_txt', type=str, default="./analysis/used_token.txt")
+parser.add_argument('--token_csv', type=str, default="./token/token_M-PA.csv")
 parser.add_argument('--out_dir', type=str, default="./output_data")
 args = parser.parse_args()
 
-used_token = []
-with open(args.token_txt, "r") as file: 
-    for line in file:
-        token = line.strip()
-        if token != '': 
-            used_token.append(token)
-
 def get_data(token):
     url = f"{root_url}/{token}_finished/mem_diff-results.json"
-    response = requests.get(url, auth=(user, password))
+    head_response = requests.head(url, auth=(user, password))
+    if head_response.status_code == 200: 
+        response = requests.get(url, auth=(user, password))
+    else: 
+        print(f"{url} not accessible. Status {head_response.status_code}")
+        return 
+    
     parsed = json.loads(response.json()["data"])
     dataProperties = parsed["dataProperties"]
     session_id = dataProperties["session_id"]
     
     data = pd.DataFrame(parsed["results"]["trials"])
     data.assign(**dataProperties)
-    
-    return session_id, data
 
-for token in used_token:
-    try:
-        session_id, data = get_data(token)
-        data.to_csv(f"{args.out_dir}/{session_id}.csv")
-    except:
-        print(f"Error while downloading {token}")
+    interaction_records = pd.DataFrame(parsed["interactionRecords"]["trials"])
+    
+    return session_id, data, interaction_records
+
+token_df = pd.read_csv(args.token_csv)
+for token in token_df.token.unique():
+    output = get_data(token)
+    if output is None:
+        continue
+    session_id, data, interaction_records = output
+    data.to_csv(f"{args.out_dir}/{session_id}.csv")
+    interaction_records.to_csv(f"{args.out_dir}/{session_id}_ir.csv")
