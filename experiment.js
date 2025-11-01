89ec7513901f69fbfee15b53a50663178cf57611
kamp <fbn.kamp@gmail.com>
2025-11-01 15:16:29 +0100

    moved create timeline to experiment js

---
+++ experiment.js
@@ -0,0 +1,201 @@
+async function createTimeline(jsPsych){
+    // Setup shortcuts
+    setupShortcuts(jsPsych);
+
+    // load session/subject/wave id
+    // const session_id = "V-PA-001"
+    const session_id = "M-PB-001-A"
+
+    // const session_id = jsPsych.data.getURLVariable('sid')
+    const version_id = session_id[0];
+    const wave_id = session_id.slice(0, 4);
+    const subject_id = parseInt(session_id.slice(4,-2), 10);
+    const backup = session_id.at(-1);
+
+    // add meta data
+    jsPsych.data.addProperties({
+        session_id : session_id,
+        version_id : version_id,
+        wave_id : wave_id,
+        subject_id : subject_id,
+        backup: backup,
+        startTime : new Date().toISOString().replace('T', ' ').slice(0, 19),
+        OS : window.navigator.platform,
+        fullscreen_mode: true,
+    });
+
+    // initialize the fullscreen tracker
+    fullscreen_tracker = new fullscreenTracker(jsPsych);
+
+    // load data
+    var input_data = await fetch(`input_data/${wave_id}/input_${session_id}.json`)
+        .then(response => response.json())
+        .catch(error => console.error(error));
+
+    if (version_id == 'M') {
+        console.log("starting memory task")
+
+        // split data into practice, wm and lm task
+        var practice_input_data = input_data.filter((trial) => trial.trial_type === "practice");
+        var wm_input_data = input_data.filter((trial) => trial.trial_type === "wm" || trial.trial_type === "catch");
+        var lm_input_data = input_data.filter((trial) => trial.trial_type === "lm");
+
+        // split into blocks
+        var wm_input_data_block1 = wm_input_data.filter((trial) => trial.wm_block_id === 1);
+        var wm_input_data_block2 = wm_input_data.filter((trial) => trial.wm_block_id === 2);
+        var wm_input_data_block3 = wm_input_data.filter((trial) => trial.wm_block_id === 3);
+
+        // overall timeline
+        var experiment_timeline = [];
+
+        // starting
+        experiment_timeline.push({
+            timeline: [
+                check_consent(jsPsych, version_id),
+                startingExperiment(jsPsych),
+                createDemographics(),
+            ],
+            name: 'exp_start'
+        })
+
+        // WM instructions
+        experiment_timeline.push({
+            timeline: [
+                createWMInstructions()
+            ],
+            name: 'wm_instructions'
+        })
+
+        // WM practice
+        experiment_timeline.push({
+            timeline: [
+                startingWMPractice(),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createWM(practice_input_data, jsPsych)
+            ],
+            name: 'wm_practice'
+        })
+
+        // WM block 1
+        experiment_timeline.push({
+            timeline: [
+                startingWM(),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createWM(wm_input_data_block1, jsPsych)
+            ],
+            name: "wm_block_1"
+        })
+
+        // WM block 2
+        experiment_timeline.push({
+            timeline: [
+                createBreak(label=1),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createWM(wm_input_data_block2, jsPsych),
+            ],
+            name: "wm_block_2"
+        })
+
+        // WM block 3
+        experiment_timeline.push({
+            timeline: [
+                createBreak(label=2),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createWM(wm_input_data_block3, jsPsych),
+                endingWM()
+            ],
+            name: "wm_block_3"
+        })
+
+        // distractor task
+        experiment_timeline.push({
+            timeline: [
+                createDistractorInstructions(),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createDistractorTask()
+            ],
+            name: "distractor_task"
+        })
+
+        // LM Instructions
+        experiment_timeline.push({
+            timeline: [
+                createLMInstructions()
+            ],
+            name: "lm_instructions"
+        })
+
+        // LM task
+        experiment_timeline.push({
+            timeline: [
+                startingLM(),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createLM(lm_input_data, jsPsych),
+            ],
+            name: "lm_task"
+        })
+
+        // Ending experiment
+        experiment_timeline.push({
+            timeline: [
+                endingExperiment(jsPsych),
+            ],
+            name: "exp_end"
+        })
+
+    } else if (version_id == 'V') {
+        console.log("starting vision task")
+        var experiment_timeline = [];
+
+        // starting
+        experiment_timeline.push({
+            timeline: [
+                check_consent(jsPsych, version_id),
+                startingExperiment(jsPsych),
+                createDemographics(),
+            ],
+            name: 'exp_start'
+        })
+
+        // INSTRUCTIONS
+        experiment_timeline.push({
+            timeline: [
+                createVisionInstructions(),
+            ],
+            name: 'instructions'
+        })
+
+        // vision task
+        experiment_timeline.push({
+            timeline: [
+                startingVisionTask(),
+                fullscreen_tracker.check(),
+                countdown(3),
+                createVisionTask(input_data, jsPsych),
+            ],
+            name: 'vision_task'
+        })
+
+        // ENDING
+        experiment_timeline.push({
+            timeline: [
+                endingExperiment(jsPsych),
+            ],
+            name: 'exp_ending'
+        })
+    }
+    return experiment_timeline;
+}
+
+// run
+async function runExperiment(jsPsych) {
+    const experiment_timeline = await createTimeline(jsPsych);
+    jsPsych.run(experiment_timeline)
+}
+
