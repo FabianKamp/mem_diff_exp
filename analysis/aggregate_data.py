08a8af857b4cd62a61c6008cd01e9b8359d8cb6c
kamp <fbn.kamp@gmail.com>
2026-03-30 12:13:20 +0200

    aggregate mixed data

--- analysis/aggregate_data.py
+++ analysis/aggregate_data.py
@@ -25,10 +25,6 @@ session_dict = {
     "M-PG": list(range(1,21)),
 }
 
-# session_dict = {
-#     "M-PG": list(range(1,21)),
-# }
-
 bin_width = .25
 bin_width_per_item = .1
 out_name = "aggregated_pilots"
@@ -49,24 +45,10 @@ for file in out_files:
     all_data.append(data)
 all_data = pd.concat(all_data) 
 
-# %%
-# load settings
-setting_files = [f"./input_data/{wave_id}/_settings.json" for wave_id in session_dict.keys()]
-setting_files = filter(os.path.exists, setting_files)
-
-load_df = []
-for file in setting_files: 
-    with open(file, "r") as f: 
-        settings = json.load(f)
-        load_df.append({
-            "wave_id": settings["wave"]["wave_id"],
-            "load": settings["memory_experiment"]["load"],
-        })
-load_df = pd.DataFrame(load_df)
-
 #%% 
 # preprocess working memory data
 # filter data
+
 output_data = all_data.loc[
     (all_data.trial_type=='wm') & 
     (all_data.condition_name!='mixed') & 
@@ -91,36 +73,54 @@ output_data = (output_data
     .reset_index()
 )
 
-# add the load
-output_data = (output_data
-    .assign(wave_id = lambda d: d.session_id.str[:4])
-    .merge(load_df, on="wave_id")
-)
+# %%
+# load settings
+def add_load_col(output_data):
+    if "load" in output_data.columns: 
+        return output_data
+    
+    setting_files = [f"./input_data/{wave_id}/_settings.json" for wave_id in session_dict.keys()]
+    setting_files = filter(os.path.exists, setting_files)
 
+    load_df = []
+    for file in setting_files: 
+        with open(file, "r") as f: 
+            settings = json.load(f)
+            load_df.append({
+                "wave_id": settings["wave"]["wave_id"],
+                "load": settings["memory_experiment"]["load"],
+            })
+    load_df = pd.DataFrame(load_df)
+    # add the load
+    output_data = (output_data
+        .assign(wave_id = lambda d: d.session_id.str[:4])
+        .merge(load_df, on="wave_id")
+    )
+    return output_data
+
+# %%
 # time to sec, take log and digitize
 def bin(time, width): 
     return np.floor(time/width)*width + width/2
 
-output_data = (output_data
-    .assign(time = lambda d: d["encoding_time"]/1e3)
-    .assign(log_time = lambda d: np.log(d["time"]))
-    .assign(bin_time = lambda d: bin(d["time"], bin_width))
-    .assign(bin_log_time = lambda d: bin(d["log_time"], bin_width))
-)
-
-# time per item
-output_data = (output_data
-    .assign(time_per_item = lambda d: d["time"]/d["load"])
-    .assign(log_time_per_item = lambda d: np.log(d["time_per_item"]))
-    .assign(bin_time_per_item = lambda d: bin(d["time_per_item"], bin_width_per_item))
-    .assign(bin_log_time_per_item = lambda d: bin(d["log_time_per_item"], bin_width_per_item))
-)
-
-# save the ouput_data
-output_data.to_csv(f"./output_data/aggregated/{out_name}.csv")
+def add_time_cols(output_data):
+    output_data = (output_data
+        .assign(time = lambda d: d["encoding_time"]/1e3)
+        .assign(log_time = lambda d: np.log(d["time"]))
+        .assign(bin_time = lambda d: bin(d["time"], bin_width))
+        .assign(bin_log_time = lambda d: bin(d["log_time"], bin_width))
+    )
+    # time per item
+    output_data = (output_data
+        .assign(time_per_item = lambda d: d["time"]/d["load"])
+        .assign(log_time_per_item = lambda d: np.log(d["time_per_item"]))
+        .assign(bin_time_per_item = lambda d: bin(d["time_per_item"], bin_width_per_item))
+        .assign(bin_log_time_per_item = lambda d: bin(d["log_time_per_item"], bin_width_per_item))
+    )
+    return output_data
 
 # %% 
-def plot_aggregate(output_data, time_col, load): 
+def plot_aggregate(output_data, time_col, load=None): 
     data = output_data.copy()
     if load is not None: 
         data = output_data.loc[output_data.load==load]
@@ -160,8 +160,10 @@ def plot_aggregate(output_data, time_col, load):
         hue="condition_name", 
         native_scale=True,
         palette=colors_palette[:2], 
+        medianprops={'color': 'red', 'linewidth': 2},
         ax=ax_box
         )
+    
     box.legend(loc="lower right")
     box.grid(alpha=.2)
     box.set_xlabel(time_col.replace("_", " "))
@@ -171,97 +173,108 @@ def plot_aggregate(output_data, time_col, load):
 
 # %% 
 # native time plot
-plot_aggregate(output_data, time_col="bin_time_per_item", load=3)
-plot_aggregate(output_data, time_col="bin_time_per_item", load=3)
-plot_aggregate(output_data, time_col="time_per_item", load=4)
 
+output_data = add_load_col(output_data)
+output_data = add_time_cols(output_data)
+plot_aggregate(output_data, time_col="bin_time_per_item")
+plot_aggregate(output_data, time_col="bin_time_per_item", load=3)
+plot_aggregate(output_data, time_col="bin_time_per_item", load=4)
 
-# %% 
-# time per item plot
-# data count 
-fig, (ax_bar, ax_box) = plt.subplots(
+# save output data
+output_data.to_csv(f"./output_data/aggregated/{out_name}.csv")
+
+# %%
+# mixed trials
+
+output_data = all_data.loc[
+    (all_data.trial_type=='wm') & 
+    (all_data.condition_name=='mixed') & 
+    (all_data.response.notna()) &
+    (all_data.phase == "recognition") 
+    ].copy()
+
+output_data.response = output_data.response.astype("Int64")
+
+# get response name (visual vs semantic)
+output_data["response_name"] = (output_data
+    .apply(lambda row: row.stimulus_left if row.response == 0 else row.stimulus_right, axis=1)
+    .str.split("/").str[-1].str[0]
+    .map({'3': "semantic", '4': "visual"})
+)
+
+# aggregate
+grouped_data = output_data.groupby(["wave_id", "encoding_time", "session_id"])["response_name"]
+output_data = pd.DataFrame({
+    "count_visual":     grouped_data.apply(lambda x: (x == 'visual').sum()),
+    "count_semantic":   grouped_data.apply(lambda x: (x == 'semantic').sum())
+}).reset_index()
+
+output_data["percent_visual"] = output_data.count_visual/(output_data.count_visual + output_data.count_semantic)
+output_data["percent_semantic"] = output_data.count_semantic/(output_data.count_visual + output_data.count_semantic)
+
+# %%
+def plot_mixed(output_data, time_col, load=None): 
+    data = output_data.copy()
+    if load is not None: 
+        data = output_data.loc[output_data.load==load]
+    
+    fig, (ax_bar, ax_box) = plt.subplots(
     nrows=2, 
     ncols=1, 
     sharex=True, 
     gridspec_kw={"height_ratios": [1, 3]},
     figsize=(10, 6)
-)
-
-session_count = (output_data
-    .groupby(["bin_time_per_item", "condition", "condition_name"])["session_id"]
-    .nunique()
-    .reset_index()
-    .rename(columns=dict(session_id="session_count"))
-)
-
-bar = sns.barplot(
-    data=session_count,
-    x="bin_time_per_item",
-    y="session_count",
-    native_scale=True,
-    ax=ax_bar,
-    legend=False, 
-    facecolor="white",
-    edgecolor="black",
-)
-bar.set_xlabel("")  
-bar.grid(alpha=.2)
-
-box = sns.boxplot(
-    data=output_data, 
-    x="bin_time_per_item", 
-    y="accuracy", 
-    hue="condition_name", 
-    native_scale=True,
-    palette=colors_palette[:2],
-    medianprops={'color': 'red', 'linewidth': 2}
     )
-box.legend(loc="lower right")
-box.grid(alpha=.2)
-box.set_xlabel("native time")
-plt.tight_layout()
 
-# %% 
-# log plot
-# data count 
-fig, (ax_bar, ax_box) = plt.subplots(
-    nrows=2, 
-    ncols=1, 
-    sharex=True, 
-    gridspec_kw={"height_ratios": [1, 3]},
-    figsize=(10, 6)
-)
-
-session_count = (output_data
-    .groupby(["bin_log_time", "condition", "condition_name"])["session_id"]
-    .nunique()
-    .reset_index()
-    .rename(columns=dict(session_id="session_count"))
-)
-
-bar = sns.barplot(
-    data=session_count,
-    x="bin_log_time",
-    y="session_count",
-    native_scale=True,
-    ax=ax_bar,
-    legend=False, 
-    facecolor="white",
-    edgecolor="black",
-)
-bar.set_xlabel("")  
-bar.grid(alpha=.2)
-bar_top = bar.secondary_xaxis("top", functions=(np.exp, np.log))
-
-box = sns.boxplot(
-    data=output_data, 
-    x="bin_log_time", 
-    y="accuracy", 
-    hue="condition_name", 
-    native_scale=True,
-    palette=colors_palette[:2]
+    session_count = (data
+        .groupby(time_col)["session_id"]
+        .nunique()
+        .reset_index()
+        .rename(columns=dict(session_id="session_count"))
     )
-box.legend(loc="lower right")
-box.grid(alpha=.2)
-box.set_xlabel("log time")
-plt.tight_layout()
+
+    bar = sns.barplot(
+        data=session_count,
+        x=time_col,
+        y="session_count",
+        native_scale=True,
+        ax=ax_bar,
+        legend=False, 
+        facecolor="white",
+        edgecolor="black",
+    )
+    bar.set_xlabel("")  
+    bar.grid(alpha=.2)
+
+    box = sns.boxplot(
+        data=data, 
+        x=time_col, 
+        y="percent_visual", 
+        native_scale=True,
+        medianprops={'color': 'green', 'linewidth': 2},
+        color = colors_palette[2],
+        ax=ax_box
+        )
+    
+    box.grid(alpha=.2)
+    box.axhline(.5, c="k", linewidth=2)
+    
+    box.set_xlabel(time_col.replace("_", " "))
+
+
+    title = f"Load = {load}" if load is not None else ""
+    bar.set_title(title)
+    plt.tight_layout()
+
+
+# %%
+output_data = add_load_col(output_data)
+output_data = add_time_cols(output_data)
+
+plot_mixed(output_data, time_col="bin_time_per_item")
+plot_mixed(output_data, time_col="bin_time_per_item", load=3)
+plot_mixed(output_data, time_col="bin_time_per_item", load=4)
+
+# save output data
+output_data.to_csv(f"./output_data/aggregated/{out_name}_mixed.csv")
+# %%
